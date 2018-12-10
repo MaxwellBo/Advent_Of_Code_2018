6054922e443e401dfbcf8b70f9d28409ee05a175
Max Bo <max@maxbo.me>
2018-12-11 08:59:36 +1100

    Bad init

---
+++ src/day_1.rs
@@ -0,0 +1,7 @@
+use utils::*
+
+fn part(n: uint8) {
+    let x = get_day_input("day_1")
+
+    println!(x)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use day_1
+
 fn main() {
     println!("Hello, world!");
+
+    day_1::part(1)
+
 }
